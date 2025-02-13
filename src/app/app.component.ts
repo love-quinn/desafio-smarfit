01a104501285836d71724e64c522c33b64baabd0
love-quinn <63768468+love-quinn@users.noreply.github.com>
2025-02-12 23:47:59 -0300

    chore: remove unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { FormsComponent } from "./components/forms/forms.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FormsComponent],
+  imports: [HeaderComponent, FormsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
