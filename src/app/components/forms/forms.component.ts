fae91ea5f35fc91b276d25b0e7e0c77d8810d016
love-quinn <63768468+love-quinn@users.noreply.github.com>
2025-02-14 19:04:37 -0300

    feat: form bind added

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
 export class FormsComponent {
   results = [];
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false,
+    });
+  }
+
+  onSubmit(): void {
+    console.log('Form submitted', this.formGroup.value);
+  }
+
+  onClean(): void {
+    this.formGroup.reset();
+  }
 }
