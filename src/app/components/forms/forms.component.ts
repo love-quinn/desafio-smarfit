263b0de9d95c2f33f270bc0a0d586d5e8738f503
love-quinn <63768468+love-quinn@users.noreply.github.com>
2025-02-14 19:32:44 -0300

    feat: add get unit service + import httpClientProvider

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { GetUnitsService } from '../../services/get-units.service';
 
 @Component({
   selector: 'app-forms',
@@ -11,9 +12,10 @@ export class FormsComponent {
   results = [];
   formGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService) {}
 
   ngOnInit(): void {
+    this.unitService.getAllUnits().subscribe(data => console.log(data));
     this.formGroup = this.formBuilder.group({
       hour: '',
       showClosed: false,
