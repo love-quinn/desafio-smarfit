63c75e977058c25cf76f98d97a4d8ba4cbe34deb
love-quinn <63768468+love-quinn@users.noreply.github.com>
2025-02-15 12:16:31 -0300

    feat: moved filter and card list to get unit service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
 import { FormsComponent } from "./components/forms/forms.component";
+import { BehaviorSubject } from 'rxjs';
+import { CardsListComponent } from "./components/cards-list/cards-list.component";
+import { CommonModule } from '@angular/common';
+import { Location } from './types/location.interface';
+import { GetUnitsService } from './services/get-units.service';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FormsComponent],
+  imports: [HeaderComponent, FormsComponent, CardsListComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'desafio-smarfit';
+  showList = new BehaviorSubject<boolean>(false);
+  unitsList:Location[] = [];
+
+  constructor(private unitService: GetUnitsService) {}
+
+  onSubmit() {
+    this.showList.next(true);
+    this.unitsList = this.unitService.getFilteredUnits();
+  }
 }
--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { GetUnitsService } from '../../services/get-units.service';
 import { Location } from '../../types/location.interface';
@@ -11,6 +11,7 @@ import { FilterUnitsService } from '../../services/filter-units.service';
   styleUrl: './forms.component.scss',
 })
 export class FormsComponent {
+  @Output() submitEvent  = new  EventEmitter();
   results: Location[] = [];
   filteredResults: Location[] = [];
   formGroup!: FormGroup;
@@ -23,8 +24,8 @@ export class FormsComponent {
 
   ngOnInit(): void {
     this.unitService.getAllUnits().subscribe((data) => {
-      this.results = data.locations;
-      this.filteredResults = data.locations;
+      this.results = data;
+      this.filteredResults = data;
     });
     this.formGroup = this.formBuilder.group({
       hour: '',
@@ -35,6 +36,9 @@ export class FormsComponent {
   onSubmit(): void {
     let { showClosed, hour } = this.formGroup.value
     this.filteredResults = this.filterUnitsService.filter(this.results, showClosed, hour)
+    this.unitService.setFilteredUnits(this.filteredResults);
+
+    this.submitEvent.emit();
   }
 
   onClean(): void {
